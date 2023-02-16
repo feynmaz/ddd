4399ac4441563f0819fcaaa128972679a1074c5a
Мазеин Николай Иванович <n.mazein@2gis.ru>
2023-02-16 10:51:05 +0700

    add memory test cases

---
+++ domain/customer/memory/memory_test.go
@@ -0,0 +1,55 @@
+package memory
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/feynmaz/ddd/domain/customer"
+
+	"github.com/feynmaz/ddd/aggregate"
+	"github.com/google/uuid"
+)
+
+func TestMemory_GetCustomer(t *testing.T) {
+	type testCase struct {
+		name        string
+		id          uuid.UUID
+		expectedErr error
+	}
+
+	cust, err := aggregate.NewCustomer("Jon")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	id := cust.GetID()
+
+	repo := MemoryRepository{
+		customers: map[uuid.UUID]aggregate.Customer{
+			id: cust,
+		},
+	}
+
+	testCases := []testCase{
+		{
+			name:        "no customer by id",
+			id:          uuid.MustParse("96fb0083-a998-41a2-8271-a3524d3e5402"),
+			expectedErr: customer.ErrCustomerNotFound,
+		},
+		{
+			name:        "customer by id",
+			id:          id,
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := repo.Get(tc.id)
+
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected error %v, got %v", tc.expectedErr, err)
+			}
+		})
+	}
+}
