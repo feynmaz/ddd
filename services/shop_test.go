e31f1fee2e78c067a03b162fc9f7c62804dd24e8
Мазеин Николай Иванович <n.mazein@2gis.ru>
2023-02-27 03:05:17 +0700

    test shop service

---
+++ services/shop_test.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"testing"
+
+	"github.com/feynmaz/ddd/aggregate"
+	"github.com/google/uuid"
+)
+
+func Test_Shop(t *testing.T) {
+	products := init_products(t)
+
+	os, err := NewOrderService(
+		WithMemoryCustomerRepository(),
+		WithMemoryProductRepository(products),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	shop, err := NewShop(WithOrderService(os))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	customer, err := aggregate.NewCustomer("percy")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if err := os.customers.Add(customer); err != nil {
+		t.Fatal(err)
+	}
+
+	order := []uuid.UUID{
+		products[0].GetID(),
+	}
+
+	_, err = shop.Order(customer.GetID(), order)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
