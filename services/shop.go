c73a2950a8ac2a1842b0053b585ac403a3c46510
Мазеин Николай Иванович <n.mazein@2gis.ru>
2023-02-27 02:56:39 +0700

    implement CreateOrder

---
+++ services/shop.go
@@ -0,0 +1,43 @@
+package services
+
+import (
+	"log"
+
+	"github.com/google/uuid"
+)
+
+type ShopConfiguration func(s *Shop) error
+
+type Shop struct {
+	OrderService   *OrderService
+	BillingService interface{}
+}
+
+func NewShop(cfgs ...ShopConfiguration) (*Shop, error) {
+	s := &Shop{}
+
+	for _, cfg := range cfgs {
+		if err := cfg(s); err != nil {
+			return nil, err
+		}
+	}
+
+	return s, nil
+}
+
+func WithOrderService(os *OrderService) ShopConfiguration {
+	return func(s *Shop) error {
+		s.OrderService = os
+		return nil
+	}
+}
+
+func (s *Shop) Order(customer uuid.UUID, products []uuid.UUID) (float64, error) {
+	total, err := s.OrderService.CreateOrder(customer, products)
+	if err != nil {
+		return 0, err
+	}
+
+	log.Printf("Bill the customer: %0.0f", total)
+	return total, nil
+}
